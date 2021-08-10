057038eb42b1fa8f829e8cd5e11d0d74c342877f
Franck Michea <franck.michea@gmail.com>
2021-08-10 23:58:06 +0200

    [lib-hooks] Add a hook to play with window size.

---
+++ src/lib/hooks/__tests__/useWindowDimensions.test.jsx
@@ -0,0 +1,148 @@
+import { mountHookWithStore } from 'tests/lib/mountHookWithStore';
+import {
+    useWindowDimensions,
+    useWindowWidthConditions,
+    windowWidthCondition,
+    windowWidthFinalCondition,
+} from 'lib/hooks/useWindowDimensions';
+import { act } from '@testing-library/react';
+
+describe('useWindowDimensions', () => {
+    const setup = (width, height) => mountHookWithStore(useWindowDimensions, {
+        windowWidth: width,
+        windowHeight: height,
+    });
+
+    test('window dimensions are returned and change is handled', () => {
+        const { getCurrentHookResult } = setup(375, 123);
+
+        expect(getCurrentHookResult()).toEqual({
+            width: 375,
+            height: 123,
+        });
+
+        act(() => {
+            const resizeEvent = document.createEvent('Event');
+            resizeEvent.initEvent('resize', true, true);
+
+            global.window.innerWidth = 789;
+            global.window.innerHeight = 453;
+            global.window.dispatchEvent(resizeEvent);
+        });
+
+        expect(getCurrentHookResult()).toEqual({
+            width: 789,
+            height: 453,
+        });
+    });
+});
+
+describe('useWindowWidthConditions', () => {
+    const realError = console.error;
+
+    beforeEach(() => {
+        console.error = jest.fn();
+    });
+
+    afterEach(() => {
+        console.error = realError;
+    });
+
+    const setup = (width, conditions) => {
+        const fn = () => useWindowWidthConditions(conditions);
+        return mountHookWithStore(fn, {
+            windowWidth: width,
+            windowHeight: 700,
+        });
+    };
+
+    test('test correct dimension is returned based on condition (final)', () => {
+        const { getCurrentHookResult } = setup(1200, [
+            windowWidthCondition(100, [1, 2]),
+            windowWidthFinalCondition([3, 4]),
+        ]);
+
+        expect(getCurrentHookResult()).toEqual([3, 4]);
+    });
+
+    test('test correct dimension is returned based on condition (first match)', () => {
+        const { getCurrentHookResult } = setup(50, [
+            windowWidthCondition(100, [1, 2]),
+            windowWidthFinalCondition([3, 4]),
+        ]);
+
+        expect(getCurrentHookResult()).toEqual([1, 2]);
+    });
+
+    test('test correct dimension is returned based on condition (middle match)', () => {
+        const { getCurrentHookResult } = setup(150, [
+            windowWidthCondition(100, [1, 2]),
+            windowWidthCondition(200, [5, 6]),
+            windowWidthFinalCondition([3, 4]),
+        ]);
+
+        expect(getCurrentHookResult()).toEqual([5, 6]);
+    });
+
+    test('test correct list but out of order is re-ordered', () => {
+        const { getCurrentHookResult } = setup(150, [
+            windowWidthCondition(200, [5, 6]),
+            windowWidthCondition(100, [1, 2]),
+            windowWidthFinalCondition([3, 4]),
+        ]);
+
+        expect(getCurrentHookResult()).toEqual([5, 6]);
+    });
+
+    test('error: no conditions provided', () => {
+        const fn = () => {
+            setup(150, []);
+        };
+
+        expect(fn).toThrow('Conditions provided are empty.');
+    });
+
+    test('error: no final case', () => {
+        const fn = () => {
+            setup(150, [
+                windowWidthCondition(200, [5, 6]),
+                windowWidthCondition(100, [1, 2]),
+            ]);
+        };
+
+        expect(fn).toThrow('Final condition provided has a limit.');
+    });
+
+    test('error: no final case', () => {
+        const fn = () => {
+            setup(150, [
+                windowWidthFinalCondition([1, 2]),
+                windowWidthFinalCondition([5, 6]),
+            ]);
+        };
+
+        expect(fn).toThrow('Condition #0 provided does not have a limit.');
+    });
+
+    test('error: no final case', () => {
+        const fn = () => {
+            setup(150, [
+                { limit: 100 },
+                windowWidthFinalCondition([5, 6]),
+            ]);
+        };
+
+        expect(fn).toThrow('Condition #0 provided does not have results.');
+    });
+
+    test('error: different sizes of conditions result', () => {
+        const fn = () => {
+            setup(150, [
+                windowWidthCondition(100, [1, 2, 3]),
+                windowWidthFinalCondition([5, 6]),
+            ]);
+        };
+
+        expect(fn).toThrow('Conditions have different sizes of results.');
+    });
+});
---
+++ src/lib/hooks/useWindowDimensions.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect, useMemo } from 'react';
+import { isNotNullOrUndefined, isNullOrUndefined } from 'lib/isNullOrUndefined';
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height,
+    };
+}
+
+export const useWindowDimensions = () => {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [setWindowDimensions]);
+
+    return windowDimensions;
+};
+
+export const windowWidthCondition = (limit, result) => ({ limit, result });
+export const windowWidthFinalCondition = (result) => ({ result });
+
+export const useWindowWidthConditions = (conditions = []) => {
+    const { width } = useWindowDimensions();
+
+    const preparedConditions = useMemo(() => {
+        if (conditions.length === 0) {
+            throw new Error('Conditions provided are empty.');
+        }
+
+        const finalCondition = conditions[conditions.length - 1];
+        if (isNotNullOrUndefined(finalCondition.limit)) {
+            throw new Error('Final condition provided has a limit.');
+        }
+
+        const resultSizes = new Set();
+
+        conditions.forEach((value, idx) => {
+            if (idx < conditions.length - 1 && isNullOrUndefined(value.limit)) {
+                throw new Error(`Condition #${idx} provided does not have a limit.`);
+            }
+
+            if (isNullOrUndefined(value.result)) {
+                throw new Error(`Condition #${idx} provided does not have results.`);
+            }
+
+            resultSizes.add(value.result.length);
+        });
+
+        if (Array.from(resultSizes).length !== 1) {
+            throw new Error('Conditions have different sizes of results.');
+        }
+
+        const result = [...conditions].sort((a, b) => {
+            if (isNullOrUndefined(a.limit)) {
+                return 1;
+            }
+            if (isNullOrUndefined(b.limit)) {
+                return -1;
+            }
+            return a.limit - b.limit;
+        });
+
+        return result;
+    }, [conditions]);
+
+    return useMemo(() => {
+        let idx = 0;
+        for (; idx < preparedConditions.length - 1; idx += 1) {
+            if (width < preparedConditions[idx].limit) {
+                return preparedConditions[idx].result;
+            }
+        }
+        return preparedConditions[idx].result;
+    }, [width, preparedConditions]);
+};
