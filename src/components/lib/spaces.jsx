c5ae465dab7826c342df18b0c3ee46934d1acb89
Franck Michea <franck.michea@gmail.com>
2021-09-23 21:23:06 +0200

    [framework] Introduce some spacing utilities.

---
+++ src/components/lib/spaces.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { styled } from 'styletron-react';
+import { isNullOrUndefined } from 'lib/isNullOrUndefined';
+
+export const VWSpace = styled('span', (props) => ({
+    width: isNullOrUndefined(props.$width) ? '1em' : props.$width,
+    display: 'inline-block',
+}));
+
+export const ThickSpace = () => <VWSpace $width="1.15em" />;
+export const ThinSpace = () => <VWSpace $width=".15em" />;
+
+export const VerticalSpace = styled('div', {
+    height: '20px',
+});
