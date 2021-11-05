a4460a017d9dfddf1be5d3a5285539efc87d6f14
Franck Michea <franck.michea@gmail.com>
2021-11-05 23:59:17 +0100

    [main] Improve how Footer displays on smaller screens.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,19 +3,30 @@ import { Layout } from 'antd';
 import { useStyletron } from 'styletron-react';
 import { GithubOutlined } from '@ant-design/icons';
 import { ExternalLink } from 'components/lib/ExternalLink';
+import { useWindowDimensions } from 'lib/hooks/useWindowDimensions';
 
 const ANTDFooter = Layout.Footer;
 
 export const Footer = () => {
     const [css] = useStyletron();
 
+    const { width } = useWindowDimensions();
+
     return (
         <ANTDFooter className={css({ textAlign: 'center' })}>
             TOOLING AVAILABLE
             {' '}
             <strong>FOR FLIGHT SIMULATION ONLY</strong>
-            . DO NOT USE FOR REAL WORLD OPERATIONS. &mdash;
-            {' '}
+            .
+            {width < 780 ? (<br />) : ' '}
+            DO NOT USE FOR REAL WORLD OPERATIONS.
+            {width < 970 ? (<br />) : (
+                <>
+                    {' '}
+                    &mdash;
+                    {' '}
+                </>
+            )}
             <GithubOutlined />
             {' '}
             <ExternalLink href="https://github.com/fmichea/flightsim-tools" noIcon>
