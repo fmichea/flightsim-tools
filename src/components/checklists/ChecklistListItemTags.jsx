a5383a586d430c80c53862983ef0efa0229362c6
Franck Michea <franck.michea@gmail.com>
2021-11-16 23:55:37 +0100

    Improve margin for tags.

--- src/components/checklists/ChecklistListItemTags.jsx
+++ src/components/checklists/ChecklistListItemTags.jsx
@@ -2,21 +2,38 @@ import React from 'react';
 
 import { Tag } from 'antd';
 import PropTypes from 'prop-types';
+import { useStyletron } from 'styletron-react';
 
 import { ChecklistDataItemTagsPropTypes } from 'components/checklists/propTypes';
 import { isNotNullOrUndefined } from 'lib/isNullOrUndefined';
 
-const tagStyle = { fontFamily: 'Monaco', fontSize: '.8em' };
+const tagStyle = {
+    fontFamily: 'Monaco',
+    fontSize: '.8em',
+};
+
+const tagStyleLast = {
+    ...tagStyle,
+    marginRight: '0',
+};
 
 export const ChecklistListItemTags = ({
     tagsData,
-}) => Array.from(tagsData)
-    .filter((tag) => !tag.hidden)
-    .map((tag) => (
-        <Tag key={tag.uid} color={tag.color} style={tagStyle}>
-            {isNotNullOrUndefined(tag.title) ? tag.title : tag.uid}
-        </Tag>
-    ));
+}) => {
+    const [css] = useStyletron();
+
+    const filteredTags = Array.from(tagsData).filter((tag) => !tag.hidden);
+
+    return filteredTags.map((tag, idx) => {
+        const isLast = idx === filteredTags.length - 1;
+
+        return (
+            <Tag key={tag.uid} color={tag.color} className={css(isLast ? tagStyleLast : tagStyle)}>
+                {isNotNullOrUndefined(tag.title) ? tag.title : tag.uid}
+            </Tag>
+        );
+    });
+};
 
 ChecklistListItemTags.propTypes = {
     tagsData: PropTypes.arrayOf(ChecklistDataItemTagsPropTypes).isRequired,
